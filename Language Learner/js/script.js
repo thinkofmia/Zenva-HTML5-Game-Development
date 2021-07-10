3b6fc75666008b0c83f87a9ced2615a4bc8def56
thinkofmia <thinkofmia@gmail.com>
2021-07-11 00:49:43 +0800

    Added question and logic for display

--- Language Learner/js/script.js
+++ Language Learner/js/script.js
@@ -1,5 +1,19 @@
 let question = {
-    title: 'gato',
+    title: 'Whats my fav animal?',
     alternatives: ['dog','cat','bird','fish'],
-    correctAnswer: 1
+    correctAnswer: 0
 }
+
+function showQuestion(q){
+    let titleDiv = document.getElementById('title');
+
+    titleDiv.textContent = q.title;
+
+    let alts = document.querySelectorAll('.alternative');
+
+    alts.forEach(function(element,index){
+        element.textContent = q.alternatives[index];
+    })
+}
+
+showQuestion(question);
