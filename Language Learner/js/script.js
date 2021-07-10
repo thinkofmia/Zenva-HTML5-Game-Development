c04e29a3052afccb3ec1f4a447d6bad3bc5d2583
thinkofmia <thinkofmia@gmail.com>
2021-07-11 01:02:07 +0800

    Added start func

--- Language Learner/js/script.js
+++ Language Learner/js/script.js
@@ -3,33 +3,38 @@ let question = {
     alternatives: ['dog','cat','bird','fish'],
     correctAnswer: 0
 }
+let alts = document.querySelectorAll('.alternative');
+let titleDiv = document.getElementById('title');
 
-function showQuestion(q){
-    let titleDiv = document.getElementById('title');
-
-    titleDiv.textContent = q.title;
-
-    let alts = document.querySelectorAll('.alternative');
+function start(){
+    
 
     alts.forEach(function(element,index){
-        element.textContent = q.alternatives[index];
-
         element.addEventListener('click', function(){
             //Check correct answer
-            if (q.correctAnswer == index){
+            //if (q.correctAnswer == index){
                 console.log('Correct Answer!');
-            }
-            else {
-                console.log('Wrong Answer!');
-            }
+            //}
+            //else {
+            //    console.log('Wrong Answer!');
+            //}
         })
-    })
+    });
+
+    showQuestion(question);
 }
 
-showQuestion(question);
+function showQuestion(q){
+    titleDiv.textContent = q.title;
+    alts.forEach(function(element,index){
+        element.textContent = q.alternatives[index];
+    });
+}
 
 let btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
     console.log('Clicked!');
 })
+
+start();
