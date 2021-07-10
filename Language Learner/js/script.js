8e1ef41636955839b6c0565f8a99ee8b63f0bdec
thinkofmia <thinkofmia@gmail.com>
2021-07-11 01:12:33 +0800

    Added bindings to methods

--- Language Learner/js/script.js
+++ Language Learner/js/script.js
@@ -3,32 +3,41 @@ let question = {
     alternatives: ['dog','cat','bird','fish'],
     correctAnswer: 0
 }
+let alts = document.querySelectorAll('.alternative');
+
 let app = {
     start: function(){
         alts.forEach(function(element,index){
             element.addEventListener('click', function(){
-                //Check correct answer
-                //if (q.correctAnswer == index){
-                    console.log('Correct Answer!');
-                //}
-                //else {
-                //    console.log('Wrong Answer!');
-                //}
-            })
-        });
+                this.checkAnswer(index);
+            }.bind(this));
+        }.bind(this));
     
         this.showQuestion(question);
     },
 
     showQuestion: function(q){
+
+        this.currQuestion = q;
+
         titleDiv.textContent = q.title;
         alts.forEach(function(element,index){
             element.textContent = q.alternatives[index];
         });
-    }
+    },
+
+    checkAnswer: function(userSelected){
+        if (this.currQuestion.correctAnswer == userSelected){
+            //correct
+            console.log("correct!");
+        }
+        else {
+            //wrong
+            console.log("wrong!");
+        }
+    },
 }
 
-let alts = document.querySelectorAll('.alternative');
 let titleDiv = document.getElementById('title');
 
 let btn = document.getElementById('btn');
