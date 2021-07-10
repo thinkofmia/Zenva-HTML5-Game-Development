448ac7d258eb6486b295c8430068e93ecd1a068f
thinkofmia <thinkofmia@gmail.com>
2021-07-11 01:05:30 +0800

    Shifted code into app structure

--- Language Learner/js/script.js
+++ Language Learner/js/script.js
@@ -3,33 +3,33 @@ let question = {
     alternatives: ['dog','cat','bird','fish'],
     correctAnswer: 0
 }
+let app = {
+    start: function(){
+        alts.forEach(function(element,index){
+            element.addEventListener('click', function(){
+                //Check correct answer
+                //if (q.correctAnswer == index){
+                    console.log('Correct Answer!');
+                //}
+                //else {
+                //    console.log('Wrong Answer!');
+                //}
+            })
+        });
+    
+        this.showQuestion(question);
+    },
+
+    showQuestion: function(q){
+        titleDiv.textContent = q.title;
+        alts.forEach(function(element,index){
+            element.textContent = q.alternatives[index];
+        });
+    }
+}
+
 let alts = document.querySelectorAll('.alternative');
 let titleDiv = document.getElementById('title');
-
-function start(){
-    
-
-    alts.forEach(function(element,index){
-        element.addEventListener('click', function(){
-            //Check correct answer
-            //if (q.correctAnswer == index){
-                console.log('Correct Answer!');
-            //}
-            //else {
-            //    console.log('Wrong Answer!');
-            //}
-        })
-    });
-
-    showQuestion(question);
-}
-
-function showQuestion(q){
-    titleDiv.textContent = q.title;
-    alts.forEach(function(element,index){
-        element.textContent = q.alternatives[index];
-    });
-}
 
 let btn = document.getElementById('btn');
 
@@ -37,4 +37,4 @@ btn.addEventListener('click', function(){
     console.log('Clicked!');
 })
 
-start();
+app.start();
