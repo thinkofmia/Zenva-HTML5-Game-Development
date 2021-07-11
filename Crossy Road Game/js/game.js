26028010137d5295efe2bd1b0cd3a6c227d3f2c1
thinkofmia <thinkofmia@gmail.com>
2021-07-11 09:40:01 +0800

    Added Rotation and Update features

--- Crossy Road Game/js/game.js
+++ Crossy Road Game/js/game.js
@@ -23,24 +23,50 @@ gameScene.create = function() {
     //bg.setPosition(gameW/2, gameH/2);
 
     //Create Player Sprite
-    let player = this.add.sprite(70, 180, 'player');
+    this.player = this.add.sprite(70, 180, 'player');
 
-    // Stretching player
-    player.setScale(0.5, 2);
+    // Scaling player
+    this.player.setScale(0.5);
 
     // create an enemy
-    let enemy1 = this.add.sprite(250, 180, 'enemy');
+    this.enemy1 = this.add.sprite(250, 180, 'enemy');
 
-    enemy1.scaleX = 2;
-    enemy1.scaleY = 2;
+    this.enemy1.scaleX = 3;
+    this.enemy1.scaleY = 3;
 
-    //create a second enemy
-    let enemy2 = this.add.sprite(450, 180, 'enemy');
-    enemy2.displayWidth = 300;
+    // Rotating using the angle of rotation in degrees
+    //this.enemy1.angle = 45;
+    //this.enemy1.setAngle(45);
 
-    // flip
-    enemy1.flipX = true;
-    enemy1.flipY = true;
+    // Rotating using angle of rotation in radians
+    // this.enemy1.setOrigin(0, 0);
+    this.enemy1.rotation = Math.PI / 4;
+    this.enemy1.setRotation(Math.PI / 4);
+    
+    console.log(this.enemy1);
+
+    //Create a second enemy
+    this.enemy2 = this.add.sprite(450, 180, 'enemy');
+    this.enemy2.displayWidth = 300;
+
+    // Flip
+    this.enemy1.flipX = true;
+    this.enemy1.flipY = true;
+  };
+
+// Updates to 60 times per second
+gameScene.update = function(){
+    // this.enemy1.x += 1;
+    
+    this.enemy1.angle += 1;
+    
+    // Check if we've reached scale of 2
+    if(this.player.scaleX < 2) {
+      // Make the player grow
+      this.player.scaleX += 0.01;
+      this.player.scaleY += 0.01;
+    }
+    
   };
 
 // Set Configuration of Game
