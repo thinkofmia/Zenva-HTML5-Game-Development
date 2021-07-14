58f01d04077b5350ecdbaa897c4e77d936b1ea0a
thinkofmia <thinkofmia@gmail.com>
2021-07-14 13:31:27 +0800

    Added text bg

--- Virtual Pet Game/js/scenes/homeScene.js
+++ Virtual Pet Game/js/scenes/homeScene.js
@@ -8,12 +8,20 @@ homeScene.create = function(){
     bg.setOrigin(0,0);
 
     //Welcome Text
-    let text = this.add.text(this.sys.game.config.width/2, this.sys.game.config.height/2, 'VIRTUAL PET', {
+    let gameW = this.sys.game.config.width;
+    let gameH = this.sys.game.config.height;
+    let text = this.add.text(gameW/2, gameH/2, 'VIRTUAL PET', {
         font: '40px Arial',
         fill: '#ffffff',
     });
 
     text.setOrigin(0.5, 0.5);
+    text.depth = 1;
+
+    //Text background
+    let textBg = this.add.graphics();
+    textBg.fillStyle(0x000000, 0.7);
+    textBg.fillRect(gameW/2 - text.width/2, gameH/2 - text.height/2, text.width, text.height);
 
     bg.on('pointerdown', function(){
         this.scene.start('Game');
