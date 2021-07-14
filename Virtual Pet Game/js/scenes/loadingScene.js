954b095f17d90f5cfb630b6a29aa309400cfd931
thinkofmia <thinkofmia@gmail.com>
2021-07-14 13:43:01 +0800

    Added loading bar

--- Virtual Pet Game/js/scenes/loadingScene.js
+++ Virtual Pet Game/js/scenes/loadingScene.js
@@ -5,6 +5,32 @@ loadingScene.preload = function(){
     //Show Logo
     let logo = this.add.sprite(this.sys.game.config.width/2, 250, 'logo');
 
+    //Progress bar background
+    let bgBar = this.add.graphics();
+
+    let barW = 150;
+    let barH = 30;
+
+    bgBar.setPosition(this.sys.game.config.width/2 - barW/2, this.sys.game.config.height/2 - barH/2);
+    bgBar.fillStyle(0xF5F5F5, 1);
+    bgBar.fillRect(0,0,barW, barH);
+
+    //Progress Bar
+    let progressBar = this.add.graphics();
+    progressBar.setPosition(this.sys.game.config.width/2 - barW/2, this.sys.game.config.height/2 - barH/2);
+
+    //Listen to progress event
+    this.load.on('progress', function(value){
+        //Clearing progress bar
+        progressBar.clear();
+
+        //Set Style
+        progressBar.fillStyle(0x9AD98D,1);
+
+        //Draw rect
+        progressBar.fillRect(0,0, value * barW, barH);
+    }, this);
+
     // load assets
     this.load.image('backyard', 'assets/images/backyard.png');
     this.load.image('apple', 'assets/images/apple.png');
@@ -19,6 +45,11 @@ loadingScene.preload = function(){
         margin: 1,
         spacing: 1
     });
+
+    //Test only
+    for (let i=0;i<100;i++){
+        this.load.image('test'+ i, 'assets/images/candy.png');
+    }
 };
 
 loadingScene.create = function(){
@@ -32,5 +63,5 @@ loadingScene.create = function(){
         repeat: 0 //Repeat forever is -1
     });
 
-    //this.scene.start('Home');
+    this.scene.start('Home');
 }
