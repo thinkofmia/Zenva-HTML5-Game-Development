761901a47df9af21134e4f6a98ea676b708d9ce2
thinkofmia <thinkofmia@gmail.com>
2021-07-15 21:42:03 +0800

    Finishing game class

--- Match 3 Game/src/Game.js
+++ Match 3 Game/src/Game.js
@@ -1,6 +1,51 @@
+const { init, GameLoop, Sprite, initPointer, track } = kontra;
+
+
 export default class Game {
     constructor(width, height){
         this.width = width;
         this.height = height;
+
+        this.init();
+    }
+
+    init(){
+        //Initialize Kontra
+        console.log('Initializing Game');
+        const { canvas, context } = init();
+        this.canvas = canvas;
+        this.context = context;
+
+        initPointer();
+
+        //Create game loop
+        this.GameLoop = GameLoop({
+            update: this.update.bind(this),
+            render: this.render.bind(this)
+        });
+
+        //Load Assets
+        this.load();
+    }
+    
+    render(){
+        //Render sprites with Kontra
+    }
+
+    update(){
+        //Update Sprites
+    }
+
+    load(){
+        //Load Game Assets
+        console.log('Loading Assets for Game');
+
+        //Start Game
+        this.start();
+    }
+    start(){
+        //Start Game loop
+        console.log('Starting Game');
+        this.GameLoop.start();
     }
 }
