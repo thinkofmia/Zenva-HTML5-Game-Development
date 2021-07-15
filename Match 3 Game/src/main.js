03c21b21aa520b14ecfbfe3d48947f19084545c4
thinkofmia <thinkofmia@gmail.com>
2021-07-15 21:19:57 +0800

    Create sprite

--- Match 3 Game/src/main.js
+++ Match 3 Game/src/main.js
@@ -3,15 +3,26 @@ import { test } from './Game.js';
 
 console.log(test);
 */
-const { init, GameLoop } = kontra;
+const { init, GameLoop, Sprite } = kontra;
 
 const { canvas, context } = init();
+
+const sprite = Sprite({
+    x: 200,
+    y: 200,
+    color: 'blue',
+    width: 50,
+    height: 20
+});
+
+console.log(sprite);
 
 const loop = GameLoop({
     update: ()=>{
         //console.log('Update');
     },
     render: ()=>{
+        sprite.render();
         //console.log('Render');
     } 
 });
