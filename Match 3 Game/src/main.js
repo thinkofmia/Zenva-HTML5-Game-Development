bd8cb95c130e9b7116e41336bcf901b8e2276095
thinkofmia <thinkofmia@gmail.com>
2021-07-15 21:27:17 +0800

    Render and update on if sprite is loaded

--- Match 3 Game/src/main.js
+++ Match 3 Game/src/main.js
@@ -7,31 +7,38 @@ const { init, GameLoop, Sprite } = kontra;
 
 const { canvas, context } = init();
 
+let sprite = null;
 const blockImage = new Image();
 blockImage.src = 'assets/images/bean_blue.png';
+blockImage.onload = () => {
+    sprite = Sprite({
+        x: 200,
+        y: 200,
+    //    color: 'blue',
+    //    width: 50,
+    //    height: 20,
+        dx: 2,
+        image: blockImage
+    });
+};
 
-const sprite = Sprite({
-    x: 200,
-    y: 200,
-//    color: 'blue',
-//    width: 50,
-//    height: 20,
-    dx: 2,
-    image: blockImage
-});
 
 console.log(sprite);
 
 const loop = GameLoop({
     update: ()=>{
-        sprite.update();
+        if (sprite){
+            sprite.update();
 
-        if (sprite.x>canvas.width){
-            sprite.x = -sprite.width;
+            if (sprite.x>canvas.width){
+                sprite.x = -sprite.width;
+            }
         }
     },
     render: ()=>{
-        sprite.render();
+        if (sprite){
+            sprite.render();
+        }
     } 
 });
 
