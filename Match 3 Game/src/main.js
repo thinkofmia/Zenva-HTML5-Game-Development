cc75e4624eeb2e21282cd87524bda4fcfb5374f6
thinkofmia <thinkofmia@gmail.com>
2021-07-15 21:15:46 +0800

    Get constant loop

--- Match 3 Game/src/main.js
+++ Match 3 Game/src/main.js
@@ -3,6 +3,17 @@ import { test } from './Game.js';
 
 console.log(test);
 */
-const { init } = kontra;
+const { init, GameLoop } = kontra;
 
 const { canvas, context } = init();
+
+const loop = GameLoop({
+    update: ()=>{
+        console.log('Update');
+    },
+    render: ()=>{
+        console.log('Render');
+    } 
+});
+
+loop.start();
