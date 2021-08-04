622ff562c6187b13e7ad6e337a8eef9add6315b6
thinkofmia <thinkofmia@gmail.com>
2021-08-04 16:59:13 +0800

    Implement Ui button functionality and map to title scene

--- MMORPG/Assets/js/classes/UiButton.js
+++ MMORPG/Assets/js/classes/UiButton.js
@@ -17,12 +17,12 @@ class UiButton extends Phaser.GameObjects.Container {
 
     createButton(){
     //Create button
-    this.button = this.add.image(0,0,'button1');
+    this.button = this.scene.add.image(0,0,'button1');
     this.button.setInteractive();
     this.button.setScale(1.4);
 
     //Create button text
-    this.buttonText = this.add.text(0,0,this.text, {fontSize: '26px', fill: '#fff'});
+    this.buttonText = this.scene.add.text(0,0,this.text, {fontSize: '26px', fill: '#fff'});
     //Center button text in UI button
     Phaser.Display.Align.In.Center(this.buttonText,this.button);
 
