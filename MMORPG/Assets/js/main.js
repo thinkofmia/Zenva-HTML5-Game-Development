7c1e4bfa27516e78f8828ba8223cc9a36f003036
thinkofmia <thinkofmia@gmail.com>
2021-08-04 15:33:59 +0800

    Initialize init and update scenes

--- MMORPG/Assets/js/main.js
+++ MMORPG/Assets/js/main.js
@@ -3,12 +3,18 @@ var config = {
     width: 800,
     height: 600,
     scene: {
+        init: init,
         preload: preload,
         create: create,
+        update: update,
     },
 };
 
 var game = new Phaser.Game(config);
+
+function init(){
+
+}
 
 function preload(){
     this.load.image('button1', 'Assets/images/ui/blue_button01.png');
@@ -17,4 +23,8 @@ function preload(){
 
 function create(){
     this.add.image(100,100,'button1');
+}
+
+function update(){
+    
 }
