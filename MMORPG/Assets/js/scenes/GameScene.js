b10e373c794a85a2afa78ff32ee10fd2e2db6b19
thinkofmia <thinkofmia@gmail.com>
2021-08-04 16:19:05 +0800

    Update funcs to Game Scene, add transitioning

--- MMORPG/Assets/js/scenes/GameScene.js
+++ MMORPG/Assets/js/scenes/GameScene.js
@@ -8,6 +8,46 @@ class GameScene extends Phaser.Scene{
     }
 
     create() {
-
+        var goldPickupAudio = this.sound.add('goldSound', {
+            loop:false,
+            volume: 0.2,
+        });
+    
+        var button = this.add.image(100,100,'button1');
+        button.setOrigin(0.5,0.5);
+    
+        this.add.sprite(300,100,'button1');
+        this.chest = this.physics.add.image(300,300,'items');
+    
+        this.wall = this.physics.add.image(500,100, 'button1');
+        this.wall.setImmovable();
+    
+        this.player = this.physics.add.image(32,32,'characters',0);
+        this.player.setScale(2);
+        this.player.body.setCollideWorldBounds(true);
+    
+        this.physics.add.collider(this.player, this.wall);
+        this.physics.add.overlap(this.player, this.chest, function(player,chest){    
+            chest.destroy();
+            goldPickupAudio.play();
+        });
+    
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+    
+    update() {
+        this.player.setVelocity(0);
+        if (this.cursors.left.isDown){
+            this.player.setVelocityX(-160);
+        }
+        else if (this.cursors.right.isDown){
+            this.player.setVelocityX(160);
+        }
+        if (this.cursors.up.isDown){
+            this.player.setVelocityY(-160);
+        }
+        else if (this.cursors.down.isDown){
+            this.player.setVelocityY(160);
+        }
     }
 }
