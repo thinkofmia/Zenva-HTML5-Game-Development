7b8a75f4f821b818e8b43146b3871f8607ec8e0a
thinkofmia <thinkofmia@gmail.com>
2021-08-07 10:17:26 +0800

    Add spawner class

--- MMORPG/Assets/js/game_manager/Spawner.js
+++ MMORPG/Assets/js/game_manager/Spawner.js
@@ -1,5 +1,39 @@
 class Spawner{
-    constructor(){
+    constructor(config, spawnLocations, addObject, deleteObject){
+        this.id = config.id;
+        this.spawnInterval = config.spawnInterval;
+        this.limit = config.limit;
+        this.objectType = config.objectType;
+        this.spawnLocations = spawnLocations;
+        this.addObject = addObject;
+        this.deleteObject = deleteObject;
+
+        this.objectsCreated = [];
+
+        this.start();
+    }
+
+    start(){
+        this.interval = setInterval(()=>{
+            if (this.objectsCreated.length<this.limit){
+                this.spawnObject();
+            }
+        }, this.spawnInterval);
+    }
+
+    spawnObject(){
+
+    }
+
+    spawnChest(){
+
+    }
+
+    pickRandomLocation(){
+
+    }
+
+    removeObject(){
         
     }
 }
